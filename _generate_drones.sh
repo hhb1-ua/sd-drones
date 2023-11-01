fc08656ccb69de8bf0ae8435e5ffbc86f1b50fca
hhb1-ua <hhb1@alu.ua.es>
2023-11-01 13:40:22 +0100

    Script para abrir y generar drones en terminales

--- _generate_drones.sh
+++ _generate_drones.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 start=0
 number=1
+network="sd-drones_engine-network"
+engine_adress="engine:9010"
+broker_adress="kafka:9092"
 
-while getopts s:n: flag
+while getopts s:n:l:e:b: flag
 do
     case $flag in
         s) start=$OPTARG;;
         n) number=$OPTARG;;
+        l) network=$OPTARG;;
+        e) engine_adress=$OPTARG;;
+        b) broker_adress=$OPTARG;;
     esac
 done
 
-for i in `seq $start $[$number - 1]`
+for i in `seq $start $[$start + $number - 1]`
 do
-    echo $i
+    # Argumentos del dron
+    container="ct_autodrone_$i"
+    identifier=$i
+    alias="autodrone_$i"
+    # Construir y ejecutar en una nueva ventana
+    docker\
+        &> /dev/null\
+        build --rm -t $container\
+        --build-arg DRONE_IDENTIFIER=$identifier\
+        --build-arg DRONE_ALIAS=$alias\
+        --build-arg ENGINE_ADRESS=$engine_adress\
+        --build-arg BROKER_ADRESS=$broker_adress\
+        ./sd_drone
+    konsole\
+        &> /dev/null\
+        --hold\
+        -e docker run --rm\
+        --network $network\
+        --network-alias alias\
+        $container\
+        &
 done
+
+# TODO: Eliminar el --hold al crear un nuevo terminal
