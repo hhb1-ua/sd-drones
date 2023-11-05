5c6c8139cfe3f92900eb2c3371b2e7c2cbe20508
hhb1-ua <hhb1@alu.ua.es>
2023-11-05 16:56:06 +0100

    Primeros pasos hacia la victoria

--- sd_drone/sd_drone.py
+++ sd_drone/sd_drone.py
@@ -1,16 +1,22 @@
 import json
 import socket
 import sys
+import os
+import threading
+import time
+import kafka
 
 ENGINE_ADRESS = ("engine", 9010)
 REGISTRY_ADRESS =  ("registry", 9020)
 BROKER_ADRESS =  ("kafka", 9092)
+CONNECTION_TIMEOUT = 20
 
 class Drone:
     def __init__(self, identifier, alias):
-        self.identifier = identifier
-        self.alias = alias
-        self.token = None
+        self.identifier = identifier    # Identificador interno
+        self.alias = alias              # Nombre interno
+        self.token = None               # Código de autentificación
+        self.partition = None           # Partición asignada
         self.x = 0
         self.y = 0
 
@@ -19,20 +25,14 @@ class Drone:
             "identifier": self.identifier,
             "alias": self.alias,
             "token": self.token,
+            "partition": self.partition,
             "position": {
                 "x": self.x,
                 "y": self.y
             }
         })
 
-    def step_toward(self, dx, dy):
-        # Comprobar que la posición está dentro del mapa
-        if dx < 0 or dy < 0 or dx >= 20 or dy >= 20:
-            raise Exception("Can't move out of bounds")
-
-        # Mover el dron
-        self.x += get_direction(self.x, dx)
-        self.y += get_direction(self.y, dy)
+    # MÉTODOS DE AUTENTIFICACIÓN
 
     def identity_register(self):
         try:
@@ -110,6 +110,66 @@ class Drone:
 
         return False
 
+    # MÉTODOS DE MOVIMIENTO
+
+    def step_toward(self, target):
+        # Comprobar que la posición está dentro del mapa
+        if target["x"] < 0 or target["y"] < 0 or target["x"] >= 20 or target["y"] >= 20:
+            return False
+
+        # Mover el dron
+        self.x += get_direction(self.x, target["x"])
+        self.y += get_direction(self.y, target["y"])
+
+        return True
+
+    # MÉTODOS DE COORDINACIÓN CON EL MOTOR
+
+    def get_drone_list(self):
+        consumer = kafka.KafkaConsumer(
+            "drone_list",
+            bootstrap_servers = [f"{BROKER_ADRESS[0]}:{BROKER_ADRESS[1]}"],
+            value_deserializer = lambda msg: msg.decode("utf-8"),
+            consumer_timeout_ms = CONNECTION_TIMEOUT * 1000
+        )
+
+        for message in consumer:
+            self.print_map(json.loads(message.value)["drone_list"])
+
+    def get_target(self):
+        consumer = kafka.KafkaConsumer(
+            bootstrap_servers = [f"{BROKER_ADRESS[0]}:{BROKER_ADRESS[1]}"],
+            value_deserializer = lambda msg: msg.decode("utf-8"),
+            consumer_timeout_ms = CONNECTION_TIMEOUT * 1000
+        )
+
+        consumer.assign([kafka.TopicPartition("drone_target", self.partition)])
+
+        for message in consumer:
+            if self.step_toward(json.loads(message.value)):
+                self.publish_data()
+
+    def publish_data(self):
+        producer = kafka.KafkaProducer(
+            bootstrap_servers = [BROKER_ADRESS],
+            value_serializer = lambda message: message.encode("utf-8")
+        )
+        producer.send(topic, value = str(self))
+
+    # MÉTODOS DE INTERFAZ DE USUARIO
+
+    def print_map(self, drone_list):
+        os.system("clear")
+
+        print(f"{self.alias} is currently at ({self.x}, {self.y})", end = "\n\n")
+        for i in range(20):
+            for j in range(20):
+                if {"x": j, "y": i} in drone_list:
+                    print("*", end = " ")
+                else:
+                    print(".", end = " ")
+            print()
+
 def get_direction(a, b):
     d = b - a
 
@@ -119,21 +179,43 @@ def get_direction(a, b):
         return -1
     return 0
 
+def get_partition_number(topic):
+    consumer = kafka.KafkaConsumer(
+        bootstrap_servers = [BROKER_ADRESS]
+    )
+    return len(consumer.partitions_for_topic(topic))
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <identifier> <alias> [<engine> <registry> <broker>]")
+        print(f"Usage: {sys.argv[0]} <identifier> <alias>")
         quit()
 
     drone = Drone(int(sys.argv[1]), str(sys.argv[2]))
-    print(f"Successfully created drone <{drone.alias}> with identifier <{drone.identifier}>")
+    print(f"Successfully created drone <{drone.alias}> with identifier <{drone.identifier}>", end = "\n\n")
 
-    try:
-        print(f"Registering drone at {REGISTRY_ADRESS[0]}:{REGISTRY_ADRESS[1]}")
+    threading.Thread(target = drone.get_drone_list, args = None).start()
+    threading.Thread(target = drone.get_target, args = None).start()
 
-        if drone.identity_register():
-            print(f"Received token <{drone.token}>")
-        else:
-            print(f"Error in registry, finishing program")
-            quit()
-    except Exception as e:
-        print(str(e))
+    # try:
+    #     print(f"Registering drone at {REGISTRY_ADRESS[0]}:{REGISTRY_ADRESS[1]}")
+    #
+    #     if drone.identity_register():
+    #         print(f"Received token <{drone.token}>")
+    #     else:
+    #         print(f"Error in registry, finishing program")
+    #         quit()
+    # except Exception as e:
+    #     print(str(e))
+    #     quit()
+
+    # try:
+    #     if input("Connect to engine? (y/N) ") is "y":
+    #         drone.main_loop(BROKER_ADRESS[0], BROKER_ADRESS[1])
+    #     else:
+    #         quit()
+    # except Exception as e:
+    #     print(str(e))
+    #     quit()
+
+
+
--- sd_engine/sd_engine.py
+++ sd_engine/sd_engine.py
@@ -1,37 +1,170 @@
 import json
 import socket
 import sys
-from kafka import KafkaProducer
-from kafka.struct import TopicPartition
+import os
+import kafka
+
+ENGINE_ADRESS = ("engine", 9010)
+REGISTRY_ADRESS =  ("registry", 9020)
+BROKER_ADRESS =  ("kafka", 9092)
+CONNECTION_TIMEOUT = 20
+
+class Listener:
+    def __init__(self, partition)
+        """
+        :partition:     Partición de escucha
+        :timestamp:     Hora de la última trama recibida
+        :status:        Estado de la conexión
+        :identifier:    Identificador externo
+        :position:      Posición actual
+        :target:        Posición final
+        """
+        self.partition = partition
+        self.timestamp = None
+        self.status = True
+        self.identifier = None
+        self.position = None
+        self.target = None
+
+class AuthenticationServer:
+    def __init__(self):
+
+
+    def start(self):
+        try:
+            while True:
+                self.handle_request()
+        except Exception as e:
+            self.socket.close()
+            raise e
+
+    def handle_request(self):
+        client_socket, client_adress = self.socket.accept()
+
+        with self.lock:
+            data = json.loads(client_socket.recv(1024).decode("utf-8"))
+            status = False
+            token = None
+
+            print(f"Request received from {client_adress[0]}:{client_adress[1]}")
+
+            try:
+                if data["operation"] == "register":
+                    # Registrar a un nuevo dron
+                    token = str(uuid.uuid4())
+                    if self.database.insert_drone(data["identifier"], data["alias"], token):
+                        status = True
+                elif data["operation"] == "delete":
+                    # Borrar un dron existente
+                    status = self.database.delete_drone(data["identifier"])
+                elif data["operation"] == "modify":
+                    # Modificar un dron existente
+                    status = self.database.modify_drone(data["identifier"], data["alias"])
+
+                response = {
+                    "accepted": status,
+                    "token": token
+                }
+                client_socket.send(json.dumps(response).encode("utf-8"))
+                client_socket.close()
+
+            except Exception as e:
+                raise e
 
 class Engine:
-    def __init__(engine_adress, broker_adress):
-        self.engine_adress = engine_adress
-        self.broker_adress = broker_adress
+    def __init__(self):
+        self.listeners = {} # Diccionario que relaciona la llave de objetivo del dron con su información
+        self.unasigned = [] # Lista de llaves de objetivo que aún no han sido asignadas
+        self.socket = None
+        self.lock = None
+        self.auth = False
 
-        # Productor de posiciones y direccionamiento
-        self.producer = KafkaProducer(
-            bootstrap_servers = [broker_adress],
-            value_serializer = lambda msg: msg.encode("utf-8")
+    def add_listener(self, key, listener):
+        if self.listeners.has_key(key):
+            return False
+
+        self.listeners[key] = listener
+        return True
+
+    def initialize_authentication_service(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.bind(ENGINE_ADRESS)
+            self.socket.listen(5)
+        except Exception as e:
+            raise e
+
+        self.lock = threading.Lock()
+        self.auth = True
+
+        while self.auth:
+            authentication_service()
+
+    def authentication_service(self):
+        client_socket, client_adress = self.socket.accept()
+
+        with self.lock:
+            data = json.loads(client_socket.recv(1024).decode("utf-8"))
+            status = False
+            token = None
+
+            print(f"Request received from {client_adress[0]}:{client_adress[1]}")
+
+            try:
+                if validate_token(data["token"]):
+                    status = True
+
+                client_socket.send(json.dumps({"accepted": status}).encode("utf-8"))
+                client_socket.close()
+            except Exception as e:
+                print(str(e))
+
+    def validate_token(token):
+        # TODO
+        return True
+
+    def publish_drone_list(self):
+        producer = kafka.KafkaProducer(
+            bootstrap_servers = [BROKER_ADRESS],
+            value_serializer = lambda message: message.encode("utf-8"))
+
+        drone_list = {}
+        for key in self.listeners:
+            drone_list[self.listeners[key]["identifier"]] = self.listeners[key]["position"]
+
+        producer.send("drone_list", value = json.dumps(drone_list))
+
+    def publish_targets(self):
+        producer = kafka.KafkaProducer(
+            bootstrap_servers = [BROKER_ADRESS],
+            value_serializer = lambda message: message.encode("utf-8"))
+
+        for key in self.listeners:
+            producer.send(
+                "drone_list",
+                value = json.dumps({
+                    "x": self.listeners[key]["target"]["x"],
+                    "y": self.listeners[key]["target"]["y"]}),
+                partition = self.listeners[key]["partition"])
+
+    def track_drones(self):
+        consumer = kafka.KafkaConsumer(
+            "drone_position",
+            bootstrap_servers = [f"{BROKER_ADRESS[0]}:{BROKER_ADRESS[1]}"],
+            value_deserializer = lambda msg: msg.decode("utf-8"),
+            consumer_timeout_ms = CONNECTION_TIMEOUT * 1000
         )
 
-        # Consumidor de desplazamiento
-        self.consumer = KafkaConsumer(
-            "drone_movement",
-            broker_servers = [broker_adress],
-            auto_offset_reset = "earliest",
-            enable_auto_commit = True,
-            # group_id = "engine",
-            value_deserializer = lambda msg: msg.decode("utf-8")
-        )
+        for message in consumer:
+            content = json.loads(message.value)
+
+            print(f"Received message from drone {content["alias"]} with identifier {content["identifier"]} at partition {message.partition}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        raise Exception("Not enough arguments")
-
-    # Conexión con Kafka
-    engine_adress = sys.argv[1]
-    broker_adress = sys.argv[2]
+        print()
+        quit()
 
     try:
         engine = new Engine()
