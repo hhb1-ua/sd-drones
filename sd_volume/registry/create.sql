38ac5ad93e731520b3fcc701c471136d243f0380
hhb1-ua <hhb1@alu.ua.es>
2023-12-12 15:04:51 +0100

    Nueva base de datos y otros detalles

--- sd_volume/registry/create.sql
+++ sd_volume/registry/create.sql
@@ -1,5 +1,10 @@
-CREATE TABLE Registry (
+CREATE TABLE Drone (
     identifier INT PRIMARY KEY,
     alias VARCHAR(128) NOT NULL,
-    token VARCHAR(128) NOT NULL
+    password VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE Token (
+    token VARCHAR(128) PRIMARY KEY,
+    expiration DATETIME NOT NULL
 );
